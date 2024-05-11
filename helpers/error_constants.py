270cd94799c573ed92cbc5250190b7cc397c54ef
oritro76 <aritra.banerjee207@gmail.com>
2024-05-11 17:29:54 +0200

    add settings, error_constatns

---
+++ helpers/error_constants.py
@@ -0,0 +1,4 @@
+ERROR_MISSING_NAME = "Missing 'name' parameter"
+ERROR_INVALID_NAME = "Invalid 'name' parameter"
+ERROR_REQUEST_LIMIT_REACHED = "Request limit reached"
+ERROR_REQUEST_LIMIT_LOW = "Request limit too low to process request"
---
+++ settings.py
@@ -0,0 +1,3 @@
+url = "https://api.nationalize.io/"
+x_rate_limit_limit_free_tier = "100"
+max_batch_size = 10
